321b1f92d1cb475a04259f95ce510dab950cbe6c
ccwhk <test@test.com>
2015-06-10 19:05:55 +0800

    Add recursive call for function helper

--- plutus-derivative/src/main/scala/org/ccw/plutus/core/derivative/option/pricing/BinomialOptionModel.scala
+++ plutus-derivative/src/main/scala/org/ccw/plutus/core/derivative/option/pricing/BinomialOptionModel.scala
@@ -3,33 +3,54 @@ package org.ccw.plutus.core.derivative.option.pricing
 import java.util.Date
 import org.ccw.plutus.core.derivative.option.model.VanillaOption
 import org.joda.time.LocalDate
+import scala.collection.mutable.Queue
 
-object BinomialOptionModel extends OptionPricingModel{
-	
-   def getImpliedVolatility (option :VanillaOption, 
-      currentDate :LocalDate, optionPrice :BigDecimal, underlyingPrice :BigDecimal) :BigDecimal ={
-     throw new Exception();
-   }
-      
-  def getOptionPrice (option :VanillaOption, 
-      currentDate :LocalDate, spotPrice :BigDecimal,  
-      volatility :BigDecimal) :BigDecimal ={
-     
+object BinomialOptionModel extends OptionPricingModel {
+
+  def getImpliedVolatility(option: VanillaOption,
+                           currentDate: LocalDate, optionPrice: BigDecimal, underlyingPrice: BigDecimal): BigDecimal = {
+    throw new Exception();
+  }
+
+  def helper(result: Queue[Double], nextList: List[Double],
+             u: Double, d: Double, i: Integer, n: Integer, isFirst: Boolean): Queue[Double] = {
+    if (i > n) {
+      result
+    } else {
+      nextList match {
+        case head :: tail => {
+          if (isFirst) {
+            result.enqueue(head * u)
+            result.enqueue(head * d)
+            helper(result, tail, u, d, i, n, false)
+          } else {
+            result.enqueue(head * d)
+            helper(result, tail, u, d, i, n, false)
+          }
+        }
+        case _ => {
+          helper(result, result.takeRight(i).toList, u, d, i + 1, n, true)
+        }
+      }
+
+    }
+  }
+
+  def getOptionPrice(option: VanillaOption,
+                     currentDate: LocalDate, spotPrice: BigDecimal,
+                     volatility: BigDecimal): BigDecimal = {
+
     if (currentDate.isBefore(option.expiryDate)) {
       // not yet expired
-      
-      
-      
+
       BigDecimal("-1")
-    } 
-    else if (currentDate.isEqual(option.expiryDate)){     
+    } else if (currentDate.isEqual(option.expiryDate)) {
       option.payOffOnExpiry(spotPrice)
-    }
-    else {
+    } else {
       // expired 
       BigDecimal("0")
     }
-    
-   }
-  
+
+  }
+
 }
--- plutus-derivative/src/test/scala/org/ccw/plutus/core/AmericanOptionSpec.scala
+++ plutus-derivative/src/test/scala/org/ccw/plutus/core/AmericanOptionSpec.scala
@@ -9,15 +9,16 @@ import org.ccw.plutus.core.model.equities.EquityType._
 import org.ccw.plutus.core.derivative.option.pricing.BinomialOptionModel
 import org.joda.time.LocalDate
 import org.scalatest.BeforeAndAfter
+import scala.collection.mutable.Queue
 
 @RunWith(classOf[JUnitRunner])
 class AmericanOptionSpec extends FlatSpec with BeforeAndAfter {
 
-  var stock :Stock = _
-  var expiryDate :LocalDate =_
-  var settlementDate :LocalDate =_
-  var americanCall :AmericanCallOption =_
-  
+  var stock: Stock = _
+  var expiryDate: LocalDate = _
+  var settlementDate: LocalDate = _
+  var americanCall: AmericanCallOption = _
+
   before {
     stock = new Stock(1, "0005", "HSBC", "HKEX")
     expiryDate = new LocalDate(2014, 1, 10)
@@ -25,43 +26,46 @@ class AmericanOptionSpec extends FlatSpec with BeforeAndAfter {
     americanCall = new AmericanCallOption(stock, BigDecimal("80"),
       settlementDate, expiryDate)
   }
-  
+
+  "recurvie call " should " work " in {
+    val initQ = Queue[Double]()
+    initQ.enqueue(5)
+    val n = 100
+    val q = BinomialOptionModel.helper(initQ, List[Double](), 2, 0.5, 1, n, true).toList
+    println(q.last)
+  }
+
   "An Amercian Call " should " be early exercisable " in {
-    
 
     val now = expiryDate.minusDays(2)
-    
+
     assertResult(true) {
       americanCall.isEarlyExercisable
     }
-    
+
     assertResult(true) {
       americanCall.isExercisableNow(now)
     }
-    
+
   }
 
-/*  
   "An American Call" should " be able to calc option price without dividend payment " in {
-    
+
     val stockPrice = BigDecimal("85")
     val volatility = BigDecimal("0.5")
-    
+
     val today = expiryDate.minusDays(2)
-    
-    val optionPrice = BinomialOptionModel.getOptionPrice(americanCall, 
-        today, stockPrice , volatility)
-        
-        // it should be non-negative
-     assert(optionPrice >= BigDecimal("0"))
-    	  
-    
-  }*/
-  
+
+    val optionPrice = BinomialOptionModel.getOptionPrice(americanCall,
+      today, stockPrice, volatility)
+
+    // it should be non-negative
+    assert(optionPrice >= BigDecimal("0"))
+
+  }
+
   "An American Call" should " be able to calc price with dividend payment " in {
-    
-    
-    
+
   }
 
 }
