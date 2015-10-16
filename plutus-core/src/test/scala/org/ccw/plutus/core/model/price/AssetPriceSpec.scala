641e3f86348e76d9b17a0f6822b3d4ea45ec5879
pccwhk <test@test.com>
2015-10-16 23:18:02 +0800

    update test case for Asset Price Spec

--- plutus-core/src/test/scala/org/ccw/plutus/core/model/price/AssetPriceSpec.scala
+++ plutus-core/src/test/scala/org/ccw/plutus/core/model/price/AssetPriceSpec.scala
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import slick.driver.H2Driver.api._
 import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.Assertions._
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -44,43 +45,57 @@ class PriceSpec extends FlatSpec with ScalaFutures {
 
   }
 
-  "Query SQL " should " be able to look up insturment " in {
+  "Query SQL " should " be able to look up data by insturment " in {
     val f = AssetPrices.findByInstrumentCode
     val c = f("0015.HK")
     val now = new java.sql.Date(System.currentTimeMillis())
     val price = new java.math.BigDecimal("0.1")
+
+    val future = db.run(c.result)
+    whenReady(future) {
+      result =>
+        {
+          result.size == 0
+        }
+    }
+
     val dataSetUp = DBIO.seq(
       AssetPrices ++= Seq(
         ("0015.HK", now, price, price, price, price)))
 
-    val future = db.run(c.result)
-
-    whenReady(future) {
-      result =>
+    whenReady(db.run(dataSetUp)) {
+      x =>
         {
-          println result
+          val future2 = db.run(c.result)
+          whenReady(future2) {
+            result =>
+              {
+                assert(result.size == 1)
+              }
+          }
         }
     }
+
   }
 
   "Price Data " should "be able to persist from Model to DB" in {
     val now = new java.sql.Date(System.currentTimeMillis())
     val price = new java.math.BigDecimal("0.2")
     val add = AssetPrices += ("0009.HK", now, price, price, price, price)
-    
+
     val future = db.run(add)
-    
+
     val symbolFilter = AssetPrices.filter(_.instrumentCode === "0009.HK")
-    
+
     val q = for {
       c <- AssetPrices if c.instrumentCode === "0009.HK"
     } yield (c.highPrice)
-    
+
     val f = db.run(q.result)
     whenReady(f) {
-      x => x foreach println
+      x => assert(x.size == 1 && x(0) == 0.2)
     }
-    
+
     //symbolFilter.update
   }
 }
--- plutus-ext/src/test/scala/org/ccw/plutus/ext/hkjc/test/HorseRaceSpec.scala
+++ plutus-ext/src/test/scala/org/ccw/plutus/ext/hkjc/test/HorseRaceSpec.scala
@@ -7,6 +7,8 @@ import org.ccw.plutus.core.web.HTML5Parser
 import java.time.LocalDate
 import scala.collection.mutable.HashMap
 import org.scalatest.junit.JUnitRunner
+import java.io.BufferedReader
+import java.io.FileReader
 
 @RunWith(classOf[JUnitRunner])
 class HorseRaceSpec extends FlatSpec with BeforeAndAfter {
@@ -23,32 +25,50 @@ class HorseRaceSpec extends FlatSpec with BeforeAndAfter {
     sb.append("&raceno=7").append("&cur=").append("220")
 
     var map = new HashMap[String, Array[String]]()
-    
+
     val temp = sb.toString()
     for (i <- 1 to 10) {
-      var url =  temp
-      if (i <10) {
+      var url = temp
+      if (i < 10) {
         url = url + 0
       }
-      
+
       val source1 = new org.xml.sax.InputSource(temp + i)
       val parser = new HTML5Parser
       val xmlNode = parser.loadXML(source1)
       val outNode = (xmlNode \\ "out")
-      
+
       outNode.toSeq foreach {
-        n =>{
-          val time = (n \ "@time").text.trim
-          val bet = n.text.trim.split(",")
-          map.put(time, bet)
-        }
+        n =>
+          {
+            val time = (n \ "@time").text.trim
+            val bet = n.text.trim.split(",")
+            map.put(time, bet)
+          }
       }
-      
+
     }
     map foreach {
-      case (k, v) =>{
+      case (k, v) => {
         println(k, v(0))
       }
+    }
+  }
+
+  ignore should "process xml" in {
+    val read = new BufferedReader(new FileReader("file-path"))
+    val source1 = new org.xml.sax.InputSource(read)
+    val parser = new HTML5Parser
+    val xmlNode = parser.loadXML(source1)
+    val outNode = (xmlNode \\ "user")
+    outNode.toSeq foreach {
+      n =>
+        {
+          val id = (n \ "@id").text.trim
+          val myType = (n \ "@type").text.trim
+          val description = (n \ "@description").text.trim
+          println(s"$myType,$id,$description")
+        }
     }
   }
 }
